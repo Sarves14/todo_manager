341b87ea04c4b4d04f5256143751ed7fddfa4eaf
sarves <rsarves14@gmail.com>
2021-02-14 17:52:19 +0530

    todo_manager with all the user management

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,20 @@
 class TodosController < ApplicationController
   def index
-    Todo.all.to_a.each { |todo| render plain: todo.to_pleasant_string }
+    if Todo.all.length > 0
+      list = Todo.all.to_a.map { |todo| todo.to_pleasant_string }
+      render plain: list.join("\n")
+    else
+      render plain: "No Todos in the database"
+    end
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(:id)
-    render plain: todo.to_pleasant_string
+    todo = Todo.find_by(id: id)
+    render plain: (todo == nil ? "No Todo with the given id" : todo.to_pleasant_string)
   end
 
-  def new
+  def create
     Todo.create!(todo_text: params[:todo_text], due_date: params[:due_date], completed: false)
   end
 
