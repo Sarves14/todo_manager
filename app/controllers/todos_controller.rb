c66691f88d6b3d924f57264a64f4055b15ab9b2c
sarves <rsarves14@gmail.com>
2021-02-14 14:46:56 +0530

    can see a single todo in /todo/id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,10 @@ class TodosController < ApplicationController
   def index
     render plain: Todo.all.to_a
   end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(:id)
+    render plain: todo.to_pleasant_string
+  end
 end
