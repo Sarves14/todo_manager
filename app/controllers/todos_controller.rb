ff9c5523bfc18cf5271cfd0db236f1775e98f0ce
sarves <rsarves14@gmail.com>
2021-02-25 13:52:52 +0530

    Added active record validation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,11 +5,16 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create!(todo_text: params[:todo_text],
-                 due_date: params[:due_date],
-                 completed: false,
-                 user_id: current_user.id)
-    redirect_to todos_path
+    new_todo = Todo.new(todo_text: params[:todo_text],
+                        due_date: params[:due_date],
+                        completed: false,
+                        user_id: current_user.id)
+    if new_todo.save
+      redirect_to todos_path
+    else
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+      redirect_to todos_path
+    end
   end
 
   def update
